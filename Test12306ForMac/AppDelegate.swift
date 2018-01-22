89f9dccb942da1e289e2af0bbe0105f2a6335f52
max lin <lindahai0512@gmail.com>
2018-01-22 11:25:40 +0800

    Initial Commit

---
+++ Test12306ForMac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Test12306ForMac
+//
+//  Created by fancymax on 2018/1/22.
+//  Copyright © 2018年 fancyApp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
